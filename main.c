a707deac198e772864fdfe7ef3ef7b34182a65fd
Toby Alden <toby.alden@gmail.com>
2014-10-19 22:15:54 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,12 +1,44 @@
 #include <ncurses.h>
 
+#define MAP_WIDTH 10
+#define MAP_HEIGHT 10
+
+void genMap();
+void draw();
+
+char map[MAP_WIDTH][MAP_HEIGHT];
+
 int main()
-{
+{	
 	initscr();
-	printw("Hello World!");
+
+    genMap();
+	draw();
 	refresh();
 	getch();
 	endwin();
 
 	return 0;
+}
+
+void genMap()
+{
+	for(int x = 0; x <= MAP_WIDTH; x++)
+	{
+		for(int y = 0; y <= MAP_HEIGHT; y++)
+		{
+			map[x][y] = 'A';
+		}
+	}
+}
+
+void draw()
+{
+    for(int x = 0; x <= MAP_WIDTH; x++)
+	{
+		for(int y = 0; y <= MAP_HEIGHT; y++)
+		{
+			mvprintw(x, y, "%c", map[x][y]);
+		}
+	}
 }
